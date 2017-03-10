57849c30f5a58b5edea420133173883c1ea9f7a4
Shaka Huang <shakalaca@gmail.com>
2017-03-10 16:24:18 +0800

    Fix typo
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,8 +18,7 @@ TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
-
-ARGET_USES_64_BIT_BINDER := true
+TARGET_USES_64_BIT_BINDER := true
 
 # Kernel
 TARGET_PREBUILT_KERNEL := device/asus/zs570kl/recovery/kernel
