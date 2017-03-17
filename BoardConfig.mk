f69a6f2d876bfe232d24d2fc1cef39a3487f9ab4
Shaka Huang <shakalaca@gmail.com>
2017-03-17 22:13:42 +0800

    Initial commit for Nougat release
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,6 @@ TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
-TARGET_USES_64_BIT_BINDER := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff androidboot.selinux=permissive
@@ -28,23 +27,22 @@ BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000
 
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_CONFIG := zs570kl-perf_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-TARGET_PREBUILT_KERNEL := device/asus/zs570kl/recovery/kernel
+TARGET_PREBUILT_KERNEL := device/asus/zs570kl/kernel
 
 TARGET_LDPRELOAD := libNimsWrap.so
 
 # Crypto
+TW_INCLUDE_CRYPTO := true
 TARGET_HW_DISK_ENCRYPTION := true
 
 # Recovery
 RECOVERY_VARIANT := twrp
-BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Partitions
+BOARD_SUPPRESS_SECURE_ERASE := true
 
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
@@ -52,18 +50,16 @@ TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
 # TWRP-Specific
 TW_THEME := portrait_hdpi
-TW_INCLUDE_CRYPTO := true
 TW_NO_EXFAT_FUSE := true
 TW_INCLUDE_NTFS_3G := true
 TW_EXTRA_LANGUAGES := true
-TW_DEFAULT_EXTERNAL_STORAGE := true
 RECOVERY_SDCARD_ON_DATA := true
-BOARD_SUPPRESS_SECURE_ERASE := true
 #TW_TARGET_USES_QCOM_BSP := true
-TW_NEW_ION_HEAP := true
+#TW_NEW_ION_HEAP := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_EXCLUDE_SUPERSU := true
 TW_SCREEN_BLANK_ON_BOOT := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
 # for fastboot boot only
 #BOARD_HAS_FLIPPED_SCREEN := true
 
@@ -75,28 +71,13 @@ TARGET_RECOVERY_DEVICE_MODULES := \
     libbinder \
     libgui \
     libui \
-    libEGL \
-    libGLES_trace \
-    libGLESv2 \
-    libprotobuf-cpp-lite \
-    libsync \
     qseecomd
 
 TW_RECOVERY_ADDITIONAL_RELINK_FILES := \
     $(OUT)/system/lib64/libbinder.so \
     $(OUT)/system/lib64/libgui.so \
     $(OUT)/system/lib64/libui.so \
-    $(OUT)/system/lib64/libEGL.so \
-    $(OUT)/system/lib64/libGLES_trace.so \
-    $(OUT)/system/lib64/libGLESv2.so \
-    $(OUT)/system/lib64/libprotobuf-cpp-lite.so \
-    $(OUT)/system/lib64/libsync.so \
     $(OUT)/system/bin/qseecomd
-
-#TARGET_UNIFIED_DEVICE := true
-#TARGET_INIT_VENDOR_LIB := libinit_zs570kl
-#TARGET_RECOVERY_DEVICE_MODULES := libinit_zs570kl
-#TARGET_LIBINIT_DEFINES_FILE := $(DEVICE_TREE)/init/init_zs570kl.cpp
 
 TARGET_UNIFIED_DEVICE := true
 TARGET_SYSTEM_PROP := device/asus/zs570kl/system.prop
