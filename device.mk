f44e9d77685c3788c6aa13d3b37b1338b366cb1e
Shaka Huang <shakalaca@gmail.com>
2017-04-16 10:38:31 +0800

    Move out gps from configs
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- device.mk
+++ device.mk
@@ -220,12 +220,12 @@ PRODUCT_PACKAGES += \
     libgnsspps
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps/etc/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/configs/gps/etc/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/configs/gps/etc/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/configs/gps/etc/lowi.conf:system/etc/lowi.conf \
-    $(LOCAL_PATH)/configs/gps/etc/sap.conf:system/etc/sap.conf \
-    $(LOCAL_PATH)/configs/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/lowi.conf:system/etc/lowi.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
 
 # IPv6
 PRODUCT_PACKAGES += \
